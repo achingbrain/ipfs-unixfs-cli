bd192f7810cf44beaed48f64ab96e70f11562b40
achingbrain <alex@achingbrain.net>
2021-04-23 12:56:40 +0100

    feat: pretty print

--- README.md
+++ README.md
@@ -2,7 +2,11 @@
 
 Parses piped data as UnixFs metadata and prints it out as a json string.
 
-```
+```console
 $ jsipfs object data QmfJMCvenrj4SKKRc48DYPxwVdS44qCUCqqtbqhJuSTWXP | unixfs
-{"type":"file","data":{"type":"Buffer","data":[]},"blockSizes":[]}
+{
+  "type": "file",
+  "blockSizes": [],
+  "mode": 420
+}
 ```
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const { UnixFS } = require('ipfs-unixfs')
+const uint8ArrayToString = require('uint8arrays/to-string')
 
 let buf = Buffer.alloc(0)
 
@@ -10,5 +11,13 @@ process.stdin.on('data', (d) => {
 
 process.stdin.on('end', () => {
   const meta = UnixFS.unmarshal(buf)
-  console.info(JSON.stringify(meta))
+  console.info(JSON.stringify({
+    type: meta.type,
+    data: meta.data ? uint8ArrayToString(meta.data, 'base64') : undefined,
+    blockSizes: meta.blockSizes,
+    hashType: meta.hashType,
+    fanout: meta.fanout,
+    mode: meta.mode,
+    mtime: meta.mtime
+  }, null, 2))
 })
